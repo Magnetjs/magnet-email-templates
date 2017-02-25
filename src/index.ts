43e49d4bb19ac1311c98746c0c51e755d3575f0f
Kieve Chua <kievechua@gmail.com>
2017-02-25 16:27:54 +0800

    Convert to typescript

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import { Module } from 'magnet-core/module'
+import { EmailTemplate } from 'email-templates'
+import * as fs from 'mz/fs'
+import * as path from 'path'
+import camelCase = require('lodash/camelCase')
+
+import defaultConfig from './config/emailTemplates'
+
+export default class MagnetEmailTemplate extends Module {
+  async setup () {
+    const config = this.prepareConfig('emailTemplates', defaultConfig)
+
+    try {
+      this.app.emailTemplates = {}
+
+      const templatesDir = path.join(process.cwd(), config.templatesDir)
+      const stat = await fs.stat(templatesDir)
+
+      if (!stat) return
+
+      const files = await fs.readdir(templatesDir)
+
+      for (const file of files) {
+        const dirPath = `${templatesDir}/${file}`
+        const dir = await fs.stat(dirPath)
+
+        if (!dir.isDirectory()) continue
+
+        this.app.emailTemplates[camelCase(file)] = new EmailTemplate(dirPath, config)
+      }
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        this.log.warn(err)
+      } else {
+        throw err
+      }
+    }
+  }
+}
