ba8431e00fa19b07881938bf2ce84013bd56590a
Kieve Chua <kievechua@gmail.com>
2017-04-10 19:19:08 +0800

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -14,25 +14,12 @@ export default class MagnetEmailTemplate extends Module {
       this.insert({})
 
       const templatesDir = path.join(this.app.config.baseDirPath, this.config.templatesDir, '/!(inky|examples)/**/*')
-      // console.log('templatesDir', templatesDir)
-      // console.log('templatesDir', templatesDir, await glob(templatesDir))
-      // const stat = await fs.stat(templatesDir)
-      //
-      // if (!stat) return
 
       const files: [String] = await glob(templatesDir)
-      // console.log('files', files)
 
       for (const file of files) {
         const parseFile = path.parse(file)
         const [, f] = parseFile.dir.split('templates')
-        // console.log('file', file, f, parseFile)
-        // console.log('file', camelCase(f))
-        // const dirPath = `${templatesDir}/${file}`
-        // const dir = await fs.stat(file)
-        //
-        // if (!dir.isDirectory()) continue
-        //
         this.app.email_templates[camelCase(f)] = new EmailTemplate(file, this.config)
       }
     } catch (err) {
